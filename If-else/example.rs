2eb695bf2bf13645c20b75b81c919f95ceca5a5a
Alessio Manai <alessio@manai.tk>
2016-03-17 15:25:52 +0100

    add if else mini example

---
+++ If-else/example.rs
@@ -0,0 +1,12 @@
+ 
+fn main(){
+	
+	let a = 2;	// x: u32 , intero a 32 bit
+	let b = 3;
+	
+	if a > b {	//rust want braces!
+		println!("{}", a);	//{} segnaposto in rust
+	} else {
+		println!("{}", b);
+	}
+}
