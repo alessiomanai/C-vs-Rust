6d35141258809381a239a161fe256cfbd55215d2
Alessio Manai <alessio@manai.tk>
2016-03-26 19:11:44 +0100

    functions example

--- Functions/main.rs
+++ Functions/main.rs
@@ -1,1 +1,15 @@
- 
+fn add_ab(a: u32, b: u32) -> u32 {		//u32 is 32 bit integer value
+	
+	a + b	//you can return a value with return or omitting semicolon
+
+} 
+
+
+fn main(){		//main function is called first like C language
+
+	let a = 2;
+	let b = 2;
+
+	println!("{}", add_ab(a,b));
+
+}
